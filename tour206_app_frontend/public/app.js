8111388575dddf56c5d130ae3c48fd55eeb336aa
mayagold <maya.gold.1991@gmail.com>
2017-09-07 21:53:32 -0700

    set up the login form and the angular script to grab form data

--- tour206_app_frontend/public/app.js
+++ tour206_app_frontend/public/app.js
@@ -3,10 +3,20 @@ console.log('hi');
 var app = angular.module('tour-app', []);
 
 app.controller('mainController', ['$http', function($http){
-  this.word = "sup";
+  const self    = this;
+
+  this.word     = "sup";
+  this.myshows  = [];
+  this.formdata = {};
+
+
+  // this is grabbing the form input data but sessions is not yet set up.
+
+  this.login    = function(){
+    console.log('logging in...');
+    console.log('Formdata: ', this.formdata);
+    
+  }
+
+
 }])
-
-
-$(()=>{
-  console.log('jquery');
-})
--- tour206_app_frontend/server.js
+++ tour206_app_frontend/server.js
@@ -3,22 +3,13 @@ const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
 const session     = require('express-session');
-const env         = require('dotenv').config();
 const PORT        = 2045;
 
 // MIDDLEWARE
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
 
-// SESSIONS CONTROLLER
-const user = require('./controllers/users_controller.js');
-app.use('/users', user);
 
 // LISTENER
 app.listen(PORT, ()=>{
